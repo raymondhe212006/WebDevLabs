8432bd87ea6bf6965d0d393da34599b32f808535
Raymond He <91344441+raymondhe212006@users.noreply.github.com>
2025-03-18 17:08:57 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -77,7 +77,7 @@ function showList() {
     document.getElementById("showListBtn").style.display = "none";
 }
 
-if (window.location.pathname.includes("index.html") || window.location.pathname === "/") {
+if (window.location.pathname.includes("index.html") || window.location.endsWith("/")) {
     greetingFunc();
     $(document).ready(function(){
         $("#readMore").click(function(){
